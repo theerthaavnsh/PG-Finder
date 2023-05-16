249f1ac92f49391e92c3bb6f707f381dcbe5cd40
theertha_avnsh <theerthaavnsh@gmail.com>
2023-05-16 19:28:55 +0530

    login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
-import Hero from './pages/hero.jsx'
+import Hero from './pages/hero.jsx';
+import Login from './login/Login'; 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-     <Hero />
-    </div>
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Hero />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/login/Login.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default funtion Login()
+{
+return(
+    <div>
+        <div>
+            <div>
+                
+            </div>
+        </div>
+    </div>
+)
+}
