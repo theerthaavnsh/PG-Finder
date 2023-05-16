4141556d5b689afafa5ea4fd701bf6685f27af58
theertha_avnsh <theerthaavnsh@gmail.com>
2023-05-16 19:40:23 +0530

    img

--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -5,7 +5,13 @@ export default function Login() {
   return (
     <div className="login">
       <div className="login-cont">
-        <div className="login-head">Hello</div>
+        <div className="login-head">
+            <h1>Welcome to your new home!</h1>
+            <P>Find your home away from home, just a click away</P>
+            <button className="login-btn">
+                <img alt="" src="google.png"></img>
+            </button>
+        </div>
         <div className="login-button"></div>
       </div>
     </div>
