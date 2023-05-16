2d49baa7a7204b024129d20c54a7ee4ca8be3f40
theertha_avnsh <theerthaavnsh@gmail.com>
2023-05-16 15:11:40 +0530

    css

--- src/pages/hero.jsx
+++ src/pages/hero.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import heroimg from "../../src/images/hero.jpg";
+import heroimg from "../../src/images/hero.png";
 import logo from '../../src/images/logo.png'
 import './hero.css'
 
@@ -16,11 +16,13 @@ export default function Hero() {
       </div>
       <div className="hero-cont">
         <div className="hero-left">
-          <h1>Discover your dream PG today</h1>
-
-         
-            <a href="/login">Join Now</a>
-       
+          <h1>Discover your dream PG today!</h1>
+          <p>Your one-stop destination for finding the perfect paying guest accommodations. 
+            Whether you're a student, working professional, or new to a city, our user-friendly platform connects you
+            with a wide range of verified PG options, making your search hassle-free and convenient. 
+            <b>Find your ideal home away from home with PG Finder today!</b> 
+          </p>
+          <a href="/login" className="join-now-button">Join Now</a>
         </div>
         <div className="hero-right">
           <img src={heroimg} alt=""></img>
