0c4442a47ee360b45a3f040db08374f115b532cd
theertha_avnsh <theerthaavnsh@gmail.com>
2023-05-17 14:16:30 +0530

    imgs

--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -1,26 +1,26 @@
 import React from "react";
 import "./login.css";
 
-
 export default function Login() {
   return (
     <div className="login">
+      <div className="login-image-left"></div>
+      <div className="login-image-right"></div>
       <div className="login-cont">
         <div className="login-head">
           <h1>Welcome To PGfind</h1>
           <p>
-            Find your perfect PG in seconds with PGFind <br></br>The ultimate
-            time-saving app for locating nearby PG!
+            Find your perfect PG in seconds with PGFind <br />
+            The ultimate time-saving app for locating nearby PG!
           </p>
-         
         </div>
         <div className="login-button">
           <button className="login-btn">
             <img
               alt=""
               src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/2008px-Google_%22G%22_Logo.svg.png"
-            ></img>
-            <p> Sign in with Google</p>
+            />
+            <p>Sign in with Google</p>
           </button>
         </div>
       </div>
