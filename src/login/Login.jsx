13254a06c6d79ca811e5a5198b088d55f1f5ac11
theertha_avnsh <theerthaavnsh@gmail.com>
2023-05-17 13:49:12 +0530

    logincss

--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -1,23 +1,28 @@
 import React from "react";
 import "./login.css";
+import google from "../../images/google.png";
 
 export default function Login() {
   return (
     <div className="login">
       <div className="login-cont">
         <div className="login-head">
-            <h1>Welcome to your new home!</h1>
-            <p>Find your home away from home, just a click away</p>
-            <div>
-            <button className="login-btn">
-                <img
-                 alt="" 
-                 src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/2008px-Google_%22G%22_Logo.svg.png"></img>
-                <p>Sign in with Google </p>
-            </button>
-            </div>
+          <h1>Welcome To PGfind</h1>
+          <p>
+            Find your perfect PG in seconds with PGFind <br></br>The ultimate
+            time-saving app for locating nearby PG!
+          </p>
+         
         </div>
-        <div className="login-button"></div>
+        <div className="login-button">
+          <button className="login-btn">
+            <img
+              alt=""
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/2008px-Google_%22G%22_Logo.svg.png"
+            ></img>
+            <p> Sign in with Google</p>
+          </button>
+        </div>
       </div>
     </div>
   );
