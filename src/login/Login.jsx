9a6a1b74b8468602e82e906acc0707394f663e3c
theertha_avnsh <theerthaavnsh@gmail.com>
2023-05-17 13:48:33 +0530

    login

--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -8,10 +8,14 @@ export default function Login() {
         <div className="login-head">
             <h1>Welcome to your new home!</h1>
             <p>Find your home away from home, just a click away</p>
-            <button className="login-button-img">
-                <img alt="" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/2008px-Google_%22G%22_Logo.svg.png"></img>
+            <div>
+            <button className="login-btn">
+                <img
+                 alt="" 
+                 src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/2008px-Google_%22G%22_Logo.svg.png"></img>
                 <p>Sign in with Google </p>
             </button>
+            </div>
         </div>
         <div className="login-button"></div>
       </div>
